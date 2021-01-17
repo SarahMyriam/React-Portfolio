8cdf33ed52d18e1f27f4e39d34abc0315d7ffb20
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-16 23:25:32 -0500

    in the component folder importanted react from react and router dom

--- react-portfolio/src/components/projects.js
+++ react-portfolio/src/components/projects.js
@@ -1,12 +1,12 @@
 import React ,{Component} from "react";
 
-class Resume extends Component{
+class Projects extends Component{
     render(){
         return(
             <div>
-                <h1>Resume</h1>
+                <h1>Projects</h1>
             </div>
         );
     }
 }
-export default Resume;
+export default Projects;
