153328b345948ed183c77856c0e5e9571ff192dc
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-17 07:25:26 -0500

    in the Header file , I added buttons and link them to the right pages, for whomever visits my site will have access to all the needed pages

---
+++ react-portfolio/src/components/Foorter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function footer(){
+    return(
+        <footer>
+            <div class="copywright">
+                <div class="up">
+                    <i class="fa fa-chevron"></i>
+                    <p class="">&copy;Coded and designed by SarahMyriamBamba 2020. All Rights Reserved</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default footer;
