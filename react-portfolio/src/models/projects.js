b8e1c2b5f8be4217a8a9c772b9ff3c33796bd494
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-17 06:08:32 -0500

    adding all my projects to the projects file in the models folder as well as the github link

---
+++ react-portfolio/src/models/projects.js
@@ -0,0 +1,43 @@
+//import React from "react";
+import Schrewd Investor from "";
+import InfluencerSearch from "";
+import Note-Taker from "";
+import Workout--Tracker from "";
+import Your-Budget-Tracker from "";
+import Schrewd Investor from "";
+
+
+const Projects =[
+    {
+        name: "Schrewd Investor",
+        img: Schrewd Investor,
+        git: "https://github.com/SarahMyriam/Shrewd-Investor"
+    },
+    {
+        name: "InfluencerSearch",
+        img: Schrewd Investor,
+        git: ""
+    },
+    {
+        name: "Note-Taker",
+        img: Schrewd Investor,
+        git: "https://github.com/SarahMyriam/Note-Taker"
+    },
+    {
+        name: "Workout--Tracker",
+        img: Schrewd Investor,
+        git: "https://github.com/SarahMyriam/Workout--Tracker"
+    },
+    {
+        name: "Your-Budget-Tracker",
+        img: Schrewd Investor,
+        git: "https://github.com/SarahMyriam/Your-Budget-Tracker"
+    },
+    {
+        name: "",
+        img: Schrewd Investor,
+        git: ""
+    },
+];
+
+export default Projects;
